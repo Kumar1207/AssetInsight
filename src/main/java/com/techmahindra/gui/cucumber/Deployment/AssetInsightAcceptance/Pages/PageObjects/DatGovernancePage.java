f38763b6df0bd462ed1741f51fb7e87341dcddef
Bhanupriya <modiyam.shanthi09@gmail.com>
2018-04-16 14:13:05 +0530

    DG Additional Questions- 3.1 - New field and Add a free text box

--- src/main/java/com/techmahindra/gui/cucumber/Deployment/AssetInsightAcceptance/Pages/PageObjects/DatGovernancePage.java
+++ src/main/java/com/techmahindra/gui/cucumber/Deployment/AssetInsightAcceptance/Pages/PageObjects/DatGovernancePage.java
@@ -1,5 +1,6 @@
 package com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Pages.PageObjects;
 
+import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Helpers.CommonMethods;
 import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Pages.Base.BasePage;
 import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.webdriver.WebDriverUtils;
 import org.openqa.selenium.JavascriptExecutor;
@@ -153,7 +154,7 @@ public class DatGovernancePage extends BasePage {
     }
 
     public boolean verifyIfTheLimitExceedsValidationMessageShouldGetDisplayed(String textvalue) {
-        //CommonMethods.ValidateAndUpdateField(newSubQuetextField, textvalue);
+        CommonMethods.ValidateAndUpdateField(newSubQuetextField, textvalue);
         return true;
     }
 
