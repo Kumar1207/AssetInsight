a5d5f403f15c02069d6a0ee97d4fd4ccb1cd346e
MP00549393 <modiyam.shanthi09@gmail.com>
2018-04-16 14:10:50 +0530

    updated TestRunner

--- src/test/java/com/techmahindra/gui/cucumber/Deployment/AssetInsightAcceptance/TestUtils/AssetInsightRunner.java
+++ src/test/java/com/techmahindra/gui/cucumber/Deployment/AssetInsightAcceptance/TestUtils/AssetInsightRunner.java
@@ -1,36 +1,37 @@
 package com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.TestUtils;
 
 import com.cucumber.listener.ExtentCucumberFormatter;
+import com.cucumber.listener.Reporter;
 import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Pages.Base.PageInstances;
+import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.managers.FileReaderManager;
+import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.webdriver.WebDriverUtils;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-
+import java.io.File;
 @RunWith(Cucumber.class)
 @CucumberOptions(format = {"pretty", "json:target/cucumber-report.json", "junit:target/cucumber-junit-report/cuc.xml"}
         , glue = {"com.techmahindra.gui.cucumber"}
         , plugin = {"com.cucumber.listener.ExtentCucumberFormatter:output/report.html"}
         , features = {"src/test/resources"}
-        //, tags = {"@AC_TechnologyPage_3.1_02"}
-
-        , tags = {"@Feature_TechnologyPage_3.1_1"}
-
+        , monochrome = true
+        , tags = {"@AC_TechnologyPage_15_02"}
 )
 
 public class AssetInsightRunner extends PageInstances {
-
     @BeforeClass
     public static void BeforeClass() throws Exception
     {
-        ExtentCucumberFormatter.initiateExtentCucumberFormatter();
+        //ExtentCucumberFormatter.initiateExtentCucumberFormatter();
 
     }
-
     @AfterClass
     public static void CreateReport() throws Exception {
-        //driver.quit();
+        Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+        WebDriverUtils.waitForElementLoading(30);
+        driver.quit();
     }
 }
 
