e3930e9f055c2e21c3db4fdfbee866b9f2736d04
MP00549393 <modiyam.shanthi09@gmail.com>
2018-04-25 18:29:07 +0530

    Update Code Line: Adding Step Def file

---
+++ src/test/java/com/techmahindra/gui/cucumber/Deployment/AssetInsightAcceptance/Stepdefinintion/ProductsPage_909_StepDef.java
@@ -0,0 +1,55 @@
+package com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Stepdefinintion;
+
+import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Pages.Base.PageInstances;
+import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Pages.PageObjects.Products_900_Page;
+import com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Pages.PageObjects.Products_909_Page;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class ProductsPage_909_StepDef extends PageInstances {
+    Products_909_Page products_909_page= PageFactory.initElements(driver, Products_909_Page.class);
+
+    @Then("^I verify the \"([^\"]*)\" should present in the Data Governance page$")
+    public void iVerifyTheShouldPresentInTheDataGovernancePage(String arg0) throws Throwable {
+        Assert.assertTrue(products_909_page.verifyNewField());
+    }
+
+    @And("^I Check the type of the new \"([^\"]*)\"$")
+    public void iCheckTheTypeOfTheNew(String arg0) throws Throwable {
+        Assert.assertTrue(products_909_page.checkTheType());
+    }
+
+    @And("^I verify the\"([^\"]*)\" should not be mandatory field$")
+    public void iVerifyTheShouldNotBeMandatoryField(String arg0) throws Throwable {
+        Assert.assertTrue(products_909_page.verifyNewFieldIsNotMandatory());
+    }
+
+    @Then("^I Verify that if the \"([^\"]*)\" exceeds validation message should get displayed$")
+    public void iVerifyThatIfTheLimitExceedsValidationMessageShouldGetDisplayed(List<String> text) throws Throwable {
+        for(String msg:text){
+            Assert.assertTrue(products_909_page.verifyValidationMessage(msg));
+        }
+
+    }
+
+    @Then("^I verify that Help text should be displayed for the new \"([^\"]*)\"$")
+    public void iVerifyThatHelpTextShouldBeDisplayedForTheNew(String arg0) throws Throwable {
+        Assert.assertTrue(products_909_page.verifyHelpText());
+    }
+
+    @Then("^I Verify the updated \"([^\"]*)\" should get displayed in the Audit History tab$")
+    public void iVerifyTheUpdatedShouldGetDisplayedInTheAuditHistoryTab(String arg0) throws Throwable {
+        Assert.assertTrue(products_909_page.verifyUpdatedFieldInAuditHistory());
+    }
+
+
+    @Then("^I verify Updated \"([^\"]*)\" should get displayed in the Data Governance tab present in the Details page$")
+    public void iVerifyUpdatedShouldGetDisplayedInTheDataGovernanceTabPresentInTheDetailsPage(String arg0) throws Throwable {
+        Assert.assertTrue(products_909_page.verifyUpdatedFieldInDataGovernance());
+    }
+}
