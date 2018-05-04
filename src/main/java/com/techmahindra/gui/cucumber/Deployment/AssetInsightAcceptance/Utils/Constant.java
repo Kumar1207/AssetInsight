1764cc5d04329f0f15dd873246d97f2038e9093c
MP00549393 <modiyam.shanthi09@gmail.com>
2018-05-04 13:06:16 +0530

    Updated Code Line:Code Check in for excel files

---
+++ src/main/java/com/techmahindra/gui/cucumber/Deployment/AssetInsightAcceptance/Utils/Constant.java
@@ -0,0 +1,18 @@
+package com.techmahindra.gui.cucumber.Deployment.AssetInsightAcceptance.Utils;
+
+    public class Constant {
+
+        public static final String Path_Application = "D:\\AssetInsight_new\\Excel\\";
+
+        public static final String File_Application = "FullExtract.xlsx";
+
+        public static final String Path_Product = "D:\\AssetInsight_new\\Excel\\";
+
+        public static final String File_Product  = "FullExtract.xlsx";
+
+
+
+//    public static final String URL = "https://assetsdev.int.thomsonreuters.com/Technology/Application/201251";
+
+    }
+
